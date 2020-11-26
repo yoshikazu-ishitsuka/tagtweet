65a50c159a7e86ea584bd1debb6ea9f6ea8d7b97
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-26 17:26:21 +0900

    update tweets_tag.rb

--- app/forms/tweets_tag.rb
+++ app/forms/tweets_tag.rb
@@ -10,7 +10,8 @@ class TweetsTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
 
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
