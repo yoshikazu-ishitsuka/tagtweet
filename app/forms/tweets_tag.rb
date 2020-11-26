99354b90dc3ae44f392c99bec8b2b7b2ee7a3411
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-26 22:38:43 +0900

    automatic shaping

--- app/forms/tweets_tag.rb
+++ app/forms/tweets_tag.rb
@@ -1,5 +1,4 @@
 class TweetsTag
-
   include ActiveModel::Model
   attr_accessor :message, :name
 
@@ -15,5 +14,4 @@ class TweetsTag
 
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
-  
 end
