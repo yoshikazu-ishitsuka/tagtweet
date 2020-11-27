de3fd2c15aba716b599344460830b5f1099b8532
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-27 20:07:08 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tweets#index'
-  resources :tweets, only: [:new, :create]
+  root to: "tweets#index"
+  resources :tweets, only: [:index, :new, :create] do
+    collection do
+      get "search"
+    end
+  end
 end
